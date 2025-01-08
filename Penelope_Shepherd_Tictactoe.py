3928f6db43a351be7a8f7c820b06ee0b74e3374b
PenelopeShepherd <pshepherd25@gcds.net>
2025-01-08 12:27:03 -0500

    Update Penelope_Shepherd_Tictactoe.py

--- Penelope_Shepherd_Tictactoe.py
+++ Penelope_Shepherd_Tictactoe.py
@@ -1,7 +1,7 @@
 import time
 import random
 mistakes = 0
-if mistakes < 3: # Unfinished feature to allow the user to only make a certain number of mistakes, not fully implemented and defective. Does not interfere with the function of the code
+if mistakes < 3:
     game = True
 while game == True:
     print("Welcome to Tic Tac Toe!")
@@ -22,24 +22,24 @@ while game == True:
         mark (str): The original mark
         box: Prints the box with the tic tac toe mark in it
         '''
-        choice = str((input(player+" "+"Please choose a spot to place your pawn" + '. (format: (ColumnLetter)(RowLetter). Ex: "AA"')).upper()) # Asks the player where they want to place thier mark
-        if choice == 'AA' or choice == 'BA' or choice == 'CA' or choice == 'AB' or choice == 'BB' or choice == 'CB' or choice == 'AC' or choice == 'BC'or choice == 'CC': # Check that the user input a valid input
-            for i in range(len(box)): # For the amount of lists in the list of lists, print a new line
-                if i != 0: # I do not want the dashed line before the first row is printed, this prevents that
-                    print('\n'+'     -------------------') # Decorative line to seperate the rows
+        choice = str((input(player+" "+"Please choose a spot to place your pawn" + '. (format: (ColumnLetter)(RowLetter). Ex: "AA"')).upper())
+        if choice == 'AA' or choice == 'BA' or choice == 'CA' or choice == 'AB' or choice == 'BB' or choice == 'CB' or choice == 'AC' or choice == 'BC'or choice == 'CC':
+            for i in range(len(box)):
+                if i != 0:
+                    print('\n'+'     -------------------')
                 else:
-                    print('\n') # Again, I do not want the dashed line before the first row is printed, so this prints a blank line there
+                    print('\n')
 
                 for j in range(len(box[i])): # For the amount of lists in box, print the box and its corresponding number, then continue on
-                    if box[i][j] == choice: # If the number that I am printing is equal to choice of the current player
+                    if box[i][j] == choice:
                         if mark == "X":
-                            box[i][j] = ' X' # Change this item of the list into the mark
+                            box[i][j] = ' X'
                             print(box[i][j], end='     ')
-                        elif mark == "O": # If the number that I am printing is equal to choice of the current player
-                            box[i][j]= " O" # Change this item of the list into the mark
-                            print(box[i][j], end='     ') 
-                    elif box[i][j] == 'X' or box[i][j] == 'O': # If the spot is already taken by a mark
-                        if mark == 'X': # Prevent the later code from switching around the marks and player because we need to repeat this function again for this player
+                        elif mark == "O":
+                            box[i][j]= " O"
+                            print(box[i][j], end='     ')
+                    elif box[i][j] == 'X' or box[i][j] == 'O':
+                        if mark == 'X':
                              mark = 'O'
                         elif mark == 'O':
                             mark = 'X'
@@ -47,31 +47,30 @@ while game == True:
                             player = 'player two'
                         elif player == 'player two':
                             player = 'player one'
-                        return mark # Leave the function so it can repeat and ask the user again
+                        return mark
                     else:
                         print(box[i][j], end='     ') # Print the the number and then a space
                 print()
-            for j in range(len(box)): # If an entire row is equal to a mark, then that mark—and its corresponding player—has won the game
-                if str.strip(box[j][1]) == mark and str.strip(box[j][2]) == mark and str.strip(box[j][3]) == mark: # Check through each row to see if they are equal to the mark
-                    print(player+" has won")
-                    time.sleep(5)
-                    exit()
-            for i in range(len(box)): # If an entire column is equal to a mark, then that mark—and its corresponding player—has won the game
-                if str.strip(box[1][i]) == mark and str.strip(box[2][i]) == mark and str.strip(box[3][i]) == mark:  # Check through each column to see if they are equal to the mark
-                    print(player+" has won")
-                    time.sleep(5)
-                    exit()
-            if str.strip(box[1][1]) == mark and str.strip(box[2][2]) == mark and str.strip(box[3][3]) == mark: # If the first diagonal is equal to the mark the coressponding player has won
+            for j in range(len(box)):
+                    if str.strip(box[j][1]) == mark and str.strip(box[j][2]) == mark and str.strip(box[j][3]) == mark:
+                        print(player+" has won")
+                        time.sleep(5)
+                        exit()
+            for i in range(len(box)):
+                    if str.strip(box[1][i]) == mark and str.strip(box[2][i]) == mark and str.strip(box[3][i]) == mark:
+                        print(player+" has won")
+                        time.sleep(5)
+                        exit()
+            if str.strip(box[1][1]) == mark and str.strip(box[2][2]) == mark and str.strip(box[3][3]) == mark:
                 print(player+" has won. The game will now close.")
                 time.sleep(5)
                 exit()
-            elif str.strip(box[1][3]) == mark and str.strip(box[2][2]) == mark and str.strip(box[3][1]) == mark:  # If the second diagonal is equal to the mark the coressponding player has won
+            elif str.strip(box[1][3]) == mark and str.strip(box[2][2]) == mark and str.strip(box[3][1]) == mark:
                 print(player+" has won. The game will now close.")
                 time.sleep(5)
                 exit()
-            elif str.strip(box[1][3])
         else:
-            if mark == 'X':  # Prevent the later code from switching around the marks and player because we need to repeat this function again for this player
+            if mark == 'X':
                 mark = 'O'
             elif mark == 'O':
                 mark = 'X'
@@ -82,22 +81,16 @@ while game == True:
             return mark
     player='user1'
     def determine_player(mistakes):
-        '''
-        Play tic tac toe and check through the box to place the X's and O's
-
-        args:
-        mistakes (int): Again a remenant of this non-imported feature
-        
-        returns:
-        player: "The name of the selected player
-        '''
         while True:
             coin_flip_choice = input("Type 1 to pick 1st player with a coin flip, type 2 to choose yourself")
             if coin_flip_choice == '1' or coin_flip_choice == '2':
                 False
                 if coin_flip_choice == '1':
                     print("User of this machine, would you like to be heads or tails?")
-                    it_doesnt_actually_matter = input("type heads or tails") # I left this part, it does not actually matter what you type because the integer will always pick the winner. This gives the illusion of choice
+                    it_doesnt_actually_matter = input("type heads or tails.")
+                    z = random.randint(1,2)
+                    if z == 1:
+                        print("User(User1), you won! You will go first.")
                         player = 'user1'
                         return player,mistakes
                     elif z == 2:
